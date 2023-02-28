195eb0a25bec8a8634e46558f06815ebf121d35b
nisarg shah <nisarg0271@gmail.com>
2023-02-27 16:01:56 -0800

    feat: Configuration file creation for FastAPI project (DI-42)

--- server/main.py
+++ server/main.py
@@ -1,8 +1,17 @@
+import os
+
+import uvicorn
+from dotenv import load_dotenv
 from fastapi import FastAPI
 
+load_dotenv(override=True)
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host=os.getenv('HOST'), port=int(os.getenv('PORT')))
