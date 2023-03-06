fbd01665993b7a798efd76b7f38edd3c03294136
tanvima <tanvima@cybage.com>
2023-03-05 19:08:53 -0800

    feat: Navbar frontend development (DI-52)

---
+++ client/src/shared/Navbar.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import LogoutIcon from '@mui/icons-material/Logout';
+import logo from "./distribute-it-logo.svg";
+const Navbar = () => {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" style={{ background: 'white', color :'gray' }}> 
+        <Toolbar> 
+           <Box
+            component="img"
+            sx={{
+            height: 64,
+            }}
+            alt="Your logo."
+            src={logo}
+        />
+          <Typography  component="div" sx={{ flexGrow: 1 }}>
+            Distribute-it
+          </Typography>
+          <Button  style={{textTransform: 'none'}} color="inherit" startIcon={<LogoutIcon />}>Sign out</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
+export default Navbar
